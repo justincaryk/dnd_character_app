80cd291b8d5396ce60b9e6a6479ef56bbed5d9f2
Justin Caryk <cary9983@gmail.com>
2021-05-24 00:07:33 -0700

    converting more classes to FCs

--- client/src/components/races/races.tsx
+++ client/src/components/races/races.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 // import "./Races.scss";
-import { appDictionary } from '../../lib/dictionary';
+import { appDictionary } from "../../lib/dictionary";
 
-class Races extends React.Component {
-    
-    render() {
-        const races = appDictionary.RACE_DATA;
+const Races: React.FC = () => {
+  const races = appDictionary.RACE_DATA;
 
-        return (
-            <div>
-                <label>Choose a Race</label>
-                <select className="form-control">
-                    <option value="" key="0000000" selected disabled>Make a selection</option>
-                    {races.map((x: any) => <option value={x.id} key={+x.id}>{x.name}</option>)}
-                </select>
-            </div>
-        );
-    }
-
-}
+  return (
+    <>
+      <label>Choose a Race</label>
+      <select className="form-control">
+        <option value="" key="0000000" selected disabled>
+          Make a selection
+        </option>
+        {races.map((x: any) => (
+          <option value={x.id} key={+x.id}>
+            {x.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
 
 export default Races;
