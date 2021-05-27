5d43d41e2a1c9a4b701c63d6bc776f3ab9e5e1ad
Justin Caryk <cary9983@gmail.com>
2021-05-26 19:40:26 -0700

    a few more FCs

--- client/src/components/descript/customBg.tsx
+++ client/src/components/descript/customBg.tsx
@@ -1,326 +1,317 @@
-import React from "react";
-import './../../scss/descript/CustomBg.scss';
-import { 
-    SkillDataType, 
-    EquipmentDataType, 
-    BackgroundFeatureDataType, 
-    LanguageType
+import React, { useState } from 'react'
+import './../../scss/descript/CustomBg.scss'
+import {
+  SkillDataType,
+  EquipmentDataType,
+  BackgroundFeatureDataType,
+  LanguageType,
 } from './../../lib/types'
 
 interface Props {
-    skillsData: SkillDataType
-    languageData: any //LanguageDataType
-    equipment: EquipmentDataType
-    bgFeatures: BackgroundFeatureDataType[]
-}
-interface State {
-    data: any
-    customOptionIsSelected: boolean
-    selectedCustomRuleOption: any
-    backgroundFeatureIsSelected: boolean
-    selectedBgFeature: any
+  skillsData: SkillDataType
+  languageData: any //LanguageDataType
+  equipment: EquipmentDataType
+  bgFeatures: BackgroundFeatureDataType[]
 }
 
 // TODO
-// for background features: 
+// for background features:
 //      sort them alphabetically, remove nonunique callers if going by feat name rather than conferring bg
 
-class CustomBgSelector extends React.Component<Props, State> {
-    handleSelection: (e: React.ChangeEvent<HTMLSelectElement>) => void
-    displayBgFeatureDetail: (e: React.ChangeEvent<HTMLSelectElement>) => void
-    buildProficiencySelectorsBlock: () => any
-    buildBackgroundFeatureInfoBlock: () => any
-    customOptionReferenceConstants: {
-        [key: string]: number
-    }
-    customOptions: {
-        id: number
-        display: string
-    }[]
+const buildSkillsBlock = (
+  numOfSelectElems: number,
+  skillsData: SkillDataType[]
+) => {
+  const dummy_array = buildDummyArrayToMapOver(numOfSelectElems)
 
-    constructor(props: Props) {
-
-        super(props)
-
-        this.customOptionReferenceConstants = {
-            'two_tools': 1,
-            'two_languages': 2,
-            'one_and_one': 3,
-        };
-
-        this.customOptions = [
-            {
-                id: this.customOptionReferenceConstants['two_tools'],
-                display: '2 Skills and 2 Tools',
-            },
-            {
-                id: this.customOptionReferenceConstants['two_languages'],
-                display: '2 Skills and 2 Languages',
-            },
-            {
-                id: this.customOptionReferenceConstants['one_and_one'],
-                display: '2 Skills, 1 Tool, and 1 Language',
-            },
-        ];
-
-        this.state = {
-            data: {
-                skillsData: this.props.skillsData,
-                languageData: this.props.languageData.ALL,
-                equipment: this.props.equipment,
-                bgFeatures: this.props.bgFeatures,
-            },
-            customOptionIsSelected: false,
-            selectedCustomRuleOption: {},
-            backgroundFeatureIsSelected: false,
-            selectedBgFeature: {},
-        }
-
-        this.handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
-            this.setState({
-                customOptionIsSelected: true,
-                selectedCustomRuleOption: event.target.value,
-            })
-        }
-
-        this.displayBgFeatureDetail = (event: React.ChangeEvent<HTMLSelectElement>) => {
-
-            const selectedBgName = event.target.value;
-
-            let bgFeatToSave;
-
-            for (const bgFeat of this.state.data.bgFeatures) {
-                if (selectedBgName == bgFeat.name) {
-                    bgFeatToSave = bgFeat;
-                    break;
-                }
-            }
-
-            this.setState({
-                backgroundFeatureIsSelected: true,
-                selectedBgFeature: bgFeatToSave ? bgFeatToSave : {},
-            })
-        }
-
-        this.buildProficiencySelectorsBlock = () => {
-
-            if (!this.state.customOptionIsSelected) {
-                return <div></div>;
-            }
-
-            let fullBlockToReturn;
-
-            const { skillsData, languageData, equipment, bgFeatures } = this.state.data;
-
-            const skills_block = _buildSkillsBlock(2, skillsData);
-            const bg_feature_block = _buildBgFeatureBlock(bgFeatures, this.displayBgFeatureDetail);
-
-            if (this.state.selectedCustomRuleOption == this.customOptionReferenceConstants['two_tools']) {
-
-                const toolsBlock = _buildToolsBlock(2, equipment);
-
-                fullBlockToReturn = (
-                    <div className="space-sequence-20">
-                        {skills_block}
-                        {toolsBlock}
-                        {bg_feature_block}
-                    </div>
-                )
-            }
-
-            if (this.state.selectedCustomRuleOption == this.customOptionReferenceConstants['two_languages']) {
-                const languagesBlock = _buildLanguagesBlock(2, languageData);
-
-                fullBlockToReturn = (
-                    <div className="space-sequence-20">
-                        {skills_block}
-                        {languagesBlock}
-                        {bg_feature_block}
-                    </div>
-                )
-            }
-
-            if (this.state.selectedCustomRuleOption == this.customOptionReferenceConstants['one_and_one']) {
-                const toolsBlock = _buildToolsBlock(1, equipment);
-                const languagesBlock = _buildLanguagesBlock(1, languageData);
-
-                fullBlockToReturn = (
-                    <div className="space-sequence-20">
-                        {skills_block}
-                        {toolsBlock}
-                        {languagesBlock}
-                        {bg_feature_block}
-                    </div>
-                )
-            }
-
-            return fullBlockToReturn;
-        }
-
-        this.buildBackgroundFeatureInfoBlock = () => {
-
-            if (this.state.backgroundFeatureIsSelected == false) {
-                return <div></div>;
-            }
-
-            return (
-                <div>
-                    <div>
-                        <div>{this.state.selectedBgFeature.name}</div>
-                        <div>
-                            <span className="text-muted">Background Feature | </span>
-                            <span className="text-muted">{this.state.selectedBgFeature.conferringBg}</span>
-                        </div>
-                    </div>
-                    <div>
-                        <div>{this.state.selectedBgFeature.description}</div>
-                    </div>
-                </div>
-            )
-        }
-    }
-
-    render() {
-
-        const customProficiencySelectorsBlock = this.buildProficiencySelectorsBlock();
-        const backgroundFeatureInfoBlock = this.buildBackgroundFeatureInfoBlock();
-
+  return (
+    <div className="space-sequence-20">
+      <div>
+        <strong>Skill Proficiencies:</strong>
+      </div>
+      {dummy_array.map((x) => {
         return (
-            <div className="space-sequence-20">
-                <div>
-                    <select className="form-control" onChange={this.handleSelection}>
-                        <option value="">Choose an Option</option>
-                        {
-                            this.customOptions.map(opt => {
-                                return (
-                                    <option key={opt.id} value={opt.id}>{opt.display}</option>
-                                )
-                            })
-                        }
-                    </select>
-                </div>
-
-                {customProficiencySelectorsBlock}
-                {backgroundFeatureInfoBlock}
-
-            </div>
-        )
-    }
-
-}
-
-
-export default CustomBgSelector;
-
-
-
-function _buildSkillsBlock(numOfSelectElems: number, skillsData: SkillDataType[]) {
-    const dummy_array = _buildDummyArrayToMapOver(numOfSelectElems);
-
-    return (
-        <div className="space-sequence-20">
-            <div><strong>Skill Proficiencies:</strong></div>
-            {
-                dummy_array.map(x => {
-                    return (
-                        <div key={'skills-dropdown-' + x}>
-                            <select className="form-control">
-                                <option value="">- Choose a Skill -</option>
-                                {
-                                    skillsData.map(skill => {
-                                        return (
-                                            <option key={skill.skill} value={skill.skill}>{skill.skill}</option>
-                                        )
-                                    })
-                                }
-                            </select>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-function _buildToolsBlock(numOfSelectElems: number, equipment: EquipmentDataType[]) {
-    const dummy_array = _buildDummyArrayToMapOver(numOfSelectElems);
-    const sortedTools = [...equipment].sort();
-
-    return (
-        <div className="space-sequence-20">
-            <div><strong>Tools Proficiencies:</strong></div>
-            {
-                dummy_array.map(x => {
-                    return (
-                        <div key={'tool-dropdown-' + x}>
-                            <select className="form-control">
-                                <option value="">- Choose a Tool -</option>
-                                {
-                                    sortedTools.map(tool => {
-                                        return <option key={tool.name} value={tool.name}>{tool.name}</option>
-                                    })
-                                }
-                            </select>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-function _buildLanguagesBlock(numOfSelectElems: number, languages: LanguageType[]) {
-    const dummy_array = _buildDummyArrayToMapOver(numOfSelectElems);
-
-    return (
-        <div className="space-sequence-20">
-            <div><strong>Languages:</strong></div>
-            {
-                dummy_array.map(x => {
-                    return (
-                        <div key={'language-dropdown-' + x}>
-                            <select className="form-control">
-                                <option value="">- Choose a Language -</option>
-                                {
-                                    languages.map(language => {
-                                        return (
-                                            <option key={language.name} value={language.name}>{language.name}</option>
-                                        )
-                                    })
-                                }
-                            </select>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-function _buildBgFeatureBlock(bgFeatures: BackgroundFeatureDataType[], displayBgFeatureDetailClosure: any) {
-
-    return (
-        <div>
-            <div><strong>Background Features:</strong></div>
-            <select className="form-control" onChange={displayBgFeatureDetailClosure}>
-                <option value="">- Choose a Background Feature -</option>
-                {
-                    bgFeatures.map(feat => {
-                        return <option value={feat.name}>{feat.name}</option>
-                    })
-                }
+          <div key={'skills-dropdown-' + x}>
+            <select className="form-control">
+              <option value="">- Choose a Skill -</option>
+              {skillsData.map((skill) => {
+                return (
+                  <option key={skill.skill} value={skill.skill}>
+                    {skill.skill}
+                  </option>
+                )
+              })}
             </select>
-        </div>
-    )
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
-function _buildDummyArrayToMapOver(elemsRequired: number) {
-    let dummyArray = [];
+const buildToolsBlock = (
+  numOfSelectElems: number,
+  equipment: EquipmentDataType[]
+) => {
+  const dummy_array = buildDummyArrayToMapOver(numOfSelectElems)
+  const sortedTools = [...equipment].sort()
 
-    for (let i = 0; i < elemsRequired; i++) {
-        dummyArray.push(i);
+  return (
+    <div className="space-sequence-20">
+      <div>
+        <strong>Tools Proficiencies:</strong>
+      </div>
+      {dummy_array.map((x) => {
+        return (
+          <div key={'tool-dropdown-' + x}>
+            <select className="form-control">
+              <option value="">- Choose a Tool -</option>
+              {sortedTools.map((tool) => {
+                return (
+                  <option key={tool.name} value={tool.name}>
+                    {tool.name}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const buildLanguagesBlock = (
+  numOfSelectElems: number,
+  languages: LanguageType[]
+) => {
+  const dummy_array = buildDummyArrayToMapOver(numOfSelectElems)
+
+  return (
+    <div className="space-sequence-20">
+      <div>
+        <strong>Languages:</strong>
+      </div>
+      {dummy_array.map((x) => {
+        return (
+          <div key={'language-dropdown-' + x}>
+            <select className="form-control">
+              <option value="">- Choose a Language -</option>
+              {languages.map((language) => {
+                return (
+                  <option key={language.name} value={language.name}>
+                    {language.name}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const buildBgFeatureBlock = (
+  bgFeatures: BackgroundFeatureDataType[],
+  displayBgFeatureDetailClosure: any
+) => {
+  return (
+    <div>
+      <div>
+        <strong>Background Features:</strong>
+      </div>
+      <select className="form-control" onChange={displayBgFeatureDetailClosure}>
+        <option value="">- Choose a Background Feature -</option>
+        {bgFeatures.map((feat) => {
+          return <option value={feat.name}>{feat.name}</option>
+        })}
+      </select>
+    </div>
+  )
+}
+
+const buildDummyArrayToMapOver = (elemsRequired: number) => {
+  let dummyArray = []
+
+  for (let i = 0; i < elemsRequired; i++) {
+    dummyArray.push(i)
+  }
+
+  return dummyArray
+}
+
+const CustomBgSelector: React.FC<Props> = ({
+  skillsData,
+  languageData,
+  equipment,
+  bgFeatures,
+}) => {
+  const customOptionReferenceConstants = {
+    two_tools: 1,
+    two_languages: 2,
+    one_and_one: 3,
+  }
+
+  const customOptions = [
+    {
+      id: customOptionReferenceConstants['two_tools'],
+      display: '2 Skills and 2 Tools',
+    },
+    {
+      id: customOptionReferenceConstants['two_languages'],
+      display: '2 Skills and 2 Languages',
+    },
+    {
+      id: customOptionReferenceConstants['one_and_one'],
+      display: '2 Skills, 1 Tool, and 1 Language',
+    },
+  ]
+
+  const [data] = useState<any>({
+    skillsData: skillsData,
+    languageData: languageData.ALL,
+    equipment: equipment,
+    bgFeatures: bgFeatures,
+  })
+
+  const [customOptionIsSelected, setCustomOptionIsSelected] = useState(false)
+  const [selectedCustomRuleOption, setSelectedCustomRuleOption] = useState<any>({})
+  const [backgroundFeatureIsSelected, setBackgroundFeatureIsSelected] =
+    useState(false)
+  const [selectedBgFeature, setSelectedBgFeature] = useState<any>({})
+
+  const handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCustomOptionIsSelected(true)
+    setSelectedCustomRuleOption(event.target.value)
+  }
+
+  const displayBgFeatureDetail = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedBgName = event.target.value
+
+    let bgFeatToSave
+
+    for (const bgFeat of data.bgFeatures) {
+      if (selectedBgName == bgFeat.name) {
+        bgFeatToSave = bgFeat
+        break
+      }
     }
 
-    return dummyArray;
+    setBackgroundFeatureIsSelected(true)
+    setSelectedBgFeature(bgFeatToSave ? bgFeatToSave : {})
+  }
 
+  const buildProficiencySelectorsBlock = () => {
+    if (!customOptionIsSelected) {
+      return null
+    }
+
+    let fullBlockToReturn
+
+    const { skillsData, languageData, equipment, bgFeatures } = data
+
+    const skills_block = buildSkillsBlock(2, skillsData)
+    const bg_feature_block = buildBgFeatureBlock(
+      bgFeatures,
+      displayBgFeatureDetail
+    )
+
+    if (
+      selectedCustomRuleOption == customOptionReferenceConstants['two_tools']
+    ) {
+      const toolsBlock = buildToolsBlock(2, equipment)
+
+      fullBlockToReturn = (
+        <div className="space-sequence-20">
+          {skills_block}
+          {toolsBlock}
+          {bg_feature_block}
+        </div>
+      )
+    }
+
+    if (
+      selectedCustomRuleOption ==
+      customOptionReferenceConstants['two_languages']
+    ) {
+      const languagesBlock = buildLanguagesBlock(2, languageData)
+
+      fullBlockToReturn = (
+        <div className="space-sequence-20">
+          {skills_block}
+          {languagesBlock}
+          {bg_feature_block}
+        </div>
+      )
+    }
+
+    if (
+      selectedCustomRuleOption == customOptionReferenceConstants['one_and_one']
+    ) {
+      const toolsBlock = buildToolsBlock(1, equipment)
+      const languagesBlock = buildLanguagesBlock(1, languageData)
+
+      fullBlockToReturn = (
+        <div className="space-sequence-20">
+          {skills_block}
+          {toolsBlock}
+          {languagesBlock}
+          {bg_feature_block}
+        </div>
+      )
+    }
+
+    return fullBlockToReturn
+  }
+
+  const buildBackgroundFeatureInfoBlock = () => {
+    if (backgroundFeatureIsSelected == false) {
+      return <div></div>
+    }
+
+    return (
+      <div>
+        <div>
+          <div>{selectedBgFeature.name}</div>
+          <div>
+            <span className="text-muted">Background Feature | </span>
+            <span className="text-muted">
+              {selectedBgFeature.conferringBg}
+            </span>
+          </div>
+        </div>
+        <div>
+          <div>{selectedBgFeature.description}</div>
+        </div>
+      </div>
+    )
+  }
+
+  const customProficiencySelectorsBlock = buildProficiencySelectorsBlock()
+  const backgroundFeatureInfoBlock = buildBackgroundFeatureInfoBlock()
+
+  return (
+    <div className="space-sequence-20">
+      <div>
+        <select className="form-control" onChange={handleSelection}>
+          <option value="">Choose an Option</option>
+          {customOptions.map((opt) => {
+            return (
+              <option key={opt.id} value={opt.id}>
+                {opt.display}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+
+      {customProficiencySelectorsBlock}
+      {backgroundFeatureInfoBlock}
+    </div>
+  )
 }
+
+export default CustomBgSelector
--- client/src/components/descript/tool.tsx
+++ client/src/components/descript/tool.tsx
@@ -1,140 +1,139 @@
-import React, { useState } from "react";
-import "./../../scss/descript/ToolProficiencies.scss";
-import { EquipmentDataType } from "./../../lib/types";
+import React, { useState } from 'react'
+import './../../scss/descript/ToolProficiencies.scss'
+import { EquipmentDataType } from './../../lib/types'
 interface ToolType {
-  name: string;
-  isAutoGranted: boolean;
+  name: string
+  isAutoGranted: boolean
 }
 
 interface Props {
-  equipment: EquipmentDataType[];
-  allToolOptions: ToolType[];
-  numberOfToolsGranted: number;
+  equipment: EquipmentDataType[]
+  allToolOptions: ToolType[]
+  numberOfToolsGranted: number
 }
 
 const getValidOptions = (
-    tools: ToolType[],
-    extraEquipment: EquipmentDataType[]
-  ) =>{
-    let standardOptions = [];
-    let instruments: string[] = [];
-    let artisanTools: string[] = [];
-    let gamingSet: string[] = [];
-  
-    for (const tool of tools) {
-      if (tool.name === "Musical Instrument") {
-        if (!instruments.length) {
-          instruments = extraEquipment
-            .filter((x) => x.type === "instrument")
-            .map((x) => x.name);
-        }
-        continue;
+  tools: ToolType[],
+  extraEquipment: EquipmentDataType[]
+) => {
+  let standardOptions = []
+  let instruments: string[] = []
+  let artisanTools: string[] = []
+  let gamingSet: string[] = []
+
+  for (const tool of tools) {
+    if (tool.name === 'Musical Instrument') {
+      if (!instruments.length) {
+        instruments = extraEquipment
+          .filter((x) => x.type === 'instrument')
+          .map((x) => x.name)
       }
-  
-      if (tool.name === "Artisan's Tools") {
-        if (!artisanTools.length) {
-          artisanTools = extraEquipment
-            .filter((x) => x.type === "artisan")
-            .map((x) => x.name);
-        }
-        continue;
-      }
-  
-      if (tool.name === "Gaming Set") {
-        if (!gamingSet.length) {
-          gamingSet = extraEquipment
-            .filter((x) => x.type === "gaming")
-            .map((x) => x.name);
-        }
-        continue;
-      }
-  
-      if (tool.isAutoGranted === false) {
-        standardOptions.push(tool.name);
-      }
+      continue
     }
-  
-    const validOptions = standardOptions.concat(
-      instruments,
-      artisanTools,
-      gamingSet
-    );
-  
-    return validOptions;
-  }
-  
-  const areToolOptionChoicesRequired = (toolOptsArray: ToolType[]) => {
-    for (const tool of toolOptsArray) {
-      if (!tool.isAutoGranted) {
-        return true;
+
+    if (tool.name === "Artisan's Tools") {
+      if (!artisanTools.length) {
+        artisanTools = extraEquipment
+          .filter((x) => x.type === 'artisan')
+          .map((x) => x.name)
       }
+      continue
     }
-    return false;
-  }
-  
-  const calcNumOfSelectElemsToGenerate = (
-    numOftoolsGranted: number,
-    toolOptionsArray: ToolType[]
-  ) => {
-    let numOftoolsAutoGranted = 0;
-    for (const tool of toolOptionsArray) {
-      if (tool.isAutoGranted) {
-        numOftoolsAutoGranted++;
+
+    if (tool.name === 'Gaming Set') {
+      if (!gamingSet.length) {
+        gamingSet = extraEquipment
+          .filter((x) => x.type === 'gaming')
+          .map((x) => x.name)
       }
+      continue
     }
-    return numOftoolsGranted - numOftoolsAutoGranted;
-  }
-  
-  const buildArrayWithThisManyItems = (elemsRequired: number) => {
-    const someArray = [];
-  
-    for (let i = 0; i < elemsRequired; i++) {
-      someArray.push(i);
+
+    if (tool.isAutoGranted === false) {
+      standardOptions.push(tool.name)
     }
-  
-    return someArray;
   }
+
+  const validOptions = standardOptions.concat(
+    instruments,
+    artisanTools,
+    gamingSet
+  )
+
+  return validOptions
+}
+
+const areToolOptionChoicesRequired = (toolOptsArray: ToolType[]) => {
+  for (const tool of toolOptsArray) {
+    if (!tool.isAutoGranted) {
+      return true
+    }
+  }
+  return false
+}
+
+const calcNumOfSelectElemsToGenerate = (
+  numOftoolsGranted: number,
+  toolOptionsArray: ToolType[]
+) => {
+  let numOftoolsAutoGranted = 0
+  for (const tool of toolOptionsArray) {
+    if (tool.isAutoGranted) {
+      numOftoolsAutoGranted++
+    }
+  }
+  return numOftoolsGranted - numOftoolsAutoGranted
+}
+
+const buildArrayWithThisManyItems = (elemsRequired: number) => {
+  const someArray = []
+
+  for (let i = 0; i < elemsRequired; i++) {
+    someArray.push(i)
+  }
+
+  return someArray
+}
 
 const ToolProficienciesSelector: React.FC<Props> = ({
   equipment,
   allToolOptions,
   numberOfToolsGranted,
 }) => {
-  const currentBgRequiresChoice = areToolOptionChoicesRequired(allToolOptions);
-  const [ validToolOptions ] = useState(getValidOptions(allToolOptions, equipment))
-  
+  const currentBgRequiresChoice = areToolOptionChoicesRequired(allToolOptions)
+  const [validToolOptions] = useState(
+    getValidOptions(allToolOptions, equipment)
+  )
+
   if (!currentBgRequiresChoice) {
-    return null;
+    return null
   }
 
   const numOfSelectElemsToGenerate = calcNumOfSelectElemsToGenerate(
     numberOfToolsGranted,
     allToolOptions
-  );
+  )
 
   const emptyArrayOfCorrectLen = buildArrayWithThisManyItems(
     numOfSelectElemsToGenerate
-  ); 
-    debugger
+  )
+
   return (
     <div className="space-sequence-20">
       {emptyArrayOfCorrectLen.map((x) => {
         return (
           <div key={x * 25}>
             <select className="form-control" defaultValue={''}>
-              <option value="">
-                - Choose a tool -
-              </option>
+              <option value="">- Choose a tool -</option>
               {validToolOptions.map((tool) => {
-                return <option key={tool}>{tool}</option>;
+                return <option key={tool}>{tool}</option>
               })}
             </select>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default ToolProficienciesSelector;
-
+export default ToolProficienciesSelector
