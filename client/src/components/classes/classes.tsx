5fc582fd58a22259b5ca0e3cf4558a2f92c387ec
Justin Caryk <cary9983@gmail.com>
2021-06-14 19:29:11 -0700

    hides classes that aren't implemented yet

--- client/src/components/classes/classes.tsx
+++ client/src/components/classes/classes.tsx
@@ -10,46 +10,46 @@ const CLASS_DATA = [
     id: 1,
     name: 'Barbarian',
   },
-  {
-    id: 2,
-    name: 'Bard',
-  },
-  {
-    id: 3,
-    name: 'Cleric',
-  },
-  {
-    id: 4,
-    name: 'Druid',
-  },
+  // {
+  //   id: 2,
+  //   name: 'Bard',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Cleric',
+  // },
+  // {
+  //   id: 4,
+  //   name: 'Druid',
+  // },
   {
     id: 5,
     name: 'Fighter',
   },
-  {
-    id: 6,
-    name: 'Monk',
-  },
+  // {
+  //   id: 6,
+  //   name: 'Monk',
+  // },
   {
     id: 7,
     name: 'Paladin',
   },
-  {
-    id: 8,
-    name: 'Ranger',
-  },
-  {
-    id: 9,
-    name: 'Rogue',
-  },
-  {
-    id: 10,
-    name: 'Sorcerer',
-  },
-  {
-    id: 11,
-    name: 'Warlock',
-  },
+  // {
+  //   id: 8,
+  //   name: 'Ranger',
+  // },
+  // {
+  //   id: 9,
+  //   name: 'Rogue',
+  // },
+  // {
+  //   id: 10,
+  //   name: 'Sorcerer',
+  // },
+  // {
+  //   id: 11,
+  //   name: 'Warlock',
+  // },
   {
     id: 12,
     name: 'Wizard',
