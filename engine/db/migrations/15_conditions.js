341023763867ecab768e24b030533d7a38cc67d6
Justin Caryk <cary9983@gmail.com>
2021-06-16 22:29:44 -0700

    conditions and diseases migration and seeds

---
+++ engine/db/migrations/15_conditions.js
@@ -0,0 +1,17 @@
+exports.up = knex => (
+    knex.schema
+        .raw(`
+            create table if not exists conditions(
+                id uuid primary key,
+                name varchar (30) not null,
+                source varchar (10),
+                page int,
+                srd boolean,
+                entries json
+            );
+        `)
+)
+
+exports.down = knex => (
+    knex.schema.dropTable('conditions')
+)
