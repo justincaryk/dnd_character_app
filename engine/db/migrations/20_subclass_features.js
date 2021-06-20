9dfc48814a10417637058d18ffead51ff02e96b2
Justin Caryk <cary9983@gmail.com>
2021-06-19 22:14:53 -0700

    subclasses added, fix for errors on subsequent seed:run calls

--- engine/db/migrations/20_subclass_features.js
+++ engine/db/migrations/20_subclass_features.js
@@ -4,17 +4,14 @@ exports.up = knex => (
             create table if not exists subclass_features (
                 id uuid primary key,
                 name text not null,
-                short_name text not null,
-                subclass_features text[],
+                source varchar(10),
+                level int,
                 class_id uuid references classes(id),
-                subclass_table_groups json[],
-                additional_spells json[],
-                spellcasting_ability varchar(10),
-                caster_progression varchar(10),
-                cantrip_progression int[],
-                spells_known_progression int[],
-                source varchar (10),
-                page int
+                subclass_short_name text,
+                subclass_source varchar(30),
+                is_class_feature_variant boolean,
+                page int,
+                entries json
             );
         `)
 )
