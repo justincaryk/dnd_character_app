9646f58d31825979567f2fe1ed39fcf9338f14b2
Justin Caryk <cary9983@gmail.com>
2021-09-13 11:49:58 -0700

    Merge branch 'main' into deploy

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -8,18 +8,17 @@ import path from 'path'
 
 const app = express()
 
-const { SERVER_PORT } = process.env
+const { PORT } = process.env
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(postgraphile)
 
-
 app.get('/', (req, res) => {
   res.sendFile(path.join('../../client/build', 'index.html'));
 });
 
-app.listen(SERVER_PORT, () =>
-  console.log(`Server ready on port ${SERVER_PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server ready on port ${PORT}`)
 )
