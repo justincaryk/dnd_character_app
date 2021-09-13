f59b43fb072112e22daed09217348548e603f97a
Justin Caryk <cary9983@gmail.com>
2021-09-13 11:27:13 -0700

    env reference issue

--- engine/src/postgraphile.ts
+++ engine/src/postgraphile.ts
@@ -1,13 +1,13 @@
 import { postgraphile } from 'postgraphile'
 
-const { DATABASE, PG_USER, PG_PASSWORD, URL, PG_PORT } = process.env
+const { DATABASE, PG_USER, PG_PASSWORD, DB_HOST, PG_PORT } = process.env
 
 export default postgraphile(
     {
         database: DATABASE,
         user: PG_USER,
         password: PG_PASSWORD,
-        host: URL,
+        host: DB_HOST,
         port: parseInt(PG_PORT),
     },
     'public',
