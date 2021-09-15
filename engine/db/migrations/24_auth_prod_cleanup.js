daaedab63acb9e8bd955742a0805bee9fd95ab04
Justin Caryk <cary9983@gmail.com>
2021-09-15 02:42:58 -0700

    one more final fix

--- engine/db/migrations/24_auth_prod_cleanup.js
+++ engine/db/migrations/24_auth_prod_cleanup.js
@@ -2,6 +2,7 @@ exports.up = knex => (
     knex.schema
         .raw(`
             GRANT USAGE ON SCHEMA public to anonymous_user;
+            GRANT USAGE ON SCHEMA public to role_minion;
         `)
 )
 
