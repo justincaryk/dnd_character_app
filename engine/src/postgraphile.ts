5a826d5d0b179c4b1251ac12a83582d059666c2b
Justin Caryk <cary9983@gmail.com>
2021-09-15 22:38:16 -0700

    turn off pg watch for production

--- engine/src/postgraphile.ts
+++ engine/src/postgraphile.ts
@@ -1,31 +1,33 @@
 import { postgraphile } from 'postgraphile'
 
-const { 
-    DATABASE, 
-    PG_USER, 
-    PG_PASSWORD, 
-    DB_HOST, 
-    PG_PORT, 
-    OWNER_CONNECTION_STRING, 
-    JWT_SECRET } = process.env
+const {
+  DATABASE,
+  PG_USER,
+  PG_PASSWORD,
+  DB_HOST,
+  PG_PORT,
+  OWNER_CONNECTION_STRING,
+  JWT_SECRET,
+  NODE_ENV,
+} = process.env
 
 export default postgraphile(
-    {
-        database: DATABASE,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        host: DB_HOST,
-        port: parseInt(PG_PORT),
-    },
-    'public',
-    {
-        jwtSecret: JWT_SECRET,
-        jwtPgTypeIdentifier: 'public.jwt_token',
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true,
-        pgDefaultRole: 'anonymous_user',
-        // ignoreRBAC: true, // Role Based Access Control (RBAC),
-        ownerConnectionString: OWNER_CONNECTION_STRING
-    }
+  {
+    database: DATABASE,
+    user: PG_USER,
+    password: PG_PASSWORD,
+    host: DB_HOST,
+    port: parseInt(PG_PORT),
+  },
+  'public',
+  {
+    jwtSecret: JWT_SECRET,
+    jwtPgTypeIdentifier: 'public.jwt_token',
+    watchPg: NODE_ENV != 'production',
+    graphiql: true,
+    enhanceGraphiql: true,
+    pgDefaultRole: 'anonymous_user',
+    // ignoreRBAC: true, // Role Based Access Control (RBAC),
+    ownerConnectionString: OWNER_CONNECTION_STRING,
+  }
 )
