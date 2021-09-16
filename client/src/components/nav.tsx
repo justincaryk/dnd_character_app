18feacd609c36a4d6c1ca16b115d78176912f845
Justin Caryk <cary9983@gmail.com>
2021-09-15 22:18:57 -0700

    lots of styling

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { LinkType } from './../lib/types'
-import './../scss/Nav.scss'
 
 interface Props {
   links: LinkType[]
@@ -8,18 +7,23 @@ interface Props {
 }
 
 const NavBar: React.FC<Props> = ({ links, isPublic }) => (
-  <div className='navbar'>
-    <div className='container relative'>
-      <div className='flex'>
+  <div className='h-22 w-full px-5 py-8 fixed top-0 bg-stone'>
+    <div className='relative'>
+      <div className='flex w-full justify-between text-sm'>
         {links.map((x) => (
           <div key={x.text}>
-            <a href={x.link}>{x.text}</a>
+            <a 
+              className='text-off-white font-roboto uppercase hover:text-hover-white hover:no-underline' 
+              href={x.link}
+            >{x.text}</a>
           </div>
         ))}
       </div>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-        <img className='w-24' src='DD-Logo-1024x487.png' alt='broken logo' />
-      </div>
+      {isPublic && (
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+          <img className='w-24' src='DD-Logo-1024x487.png' alt='broken logo' />
+        </div>
+      )}
     </div>
   </div>
 )
