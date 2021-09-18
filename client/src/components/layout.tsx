7a16139343e1cb7c6438334df110db1be6436aeb
Justin Caryk <cary9983@gmail.com>
2021-09-18 00:42:53 -0700

    okay merge issues squashed.

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,20 +1,14 @@
 import classnames from 'classnames'
 import React from 'react'
-import { useLocation } from 'react-router'
-
 interface Props {
   children?: any
 }
 const Layout: React.FC<Props> = ({ children }) => {
-  const location = useLocation()
-  
+
   return (
     <div
       className={classnames({
-        'pb-40 fixed h-full w-full bg-no-repeat bg-cover overflow-scroll':true,
-        // 'pt-24': location.pathname === '/create/sheet',
-        // 'pt-40': location.pathname !== '/create/sheet'
-
+        'pb-40 fixed h-full w-full bg-no-repeat bg-cover overflow-scroll':true
       })}
       style={{ backgroundImage: "url('/spooky_bg.jpeg')" }}
     >
--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LinkType } from './../lib/types'
 import { useLocation, useHistory } from 'react-router-dom'
+import classnames from 'classnames'
 
 interface Props {
   links: LinkType[]
@@ -14,55 +15,66 @@ const NavBar: React.FC<Props> = ({ links, isPublic, signout }) => {
   const history = useHistory()
 
   return (
-    <div className='h-38 w-full bg-stone z-10'>
-      {!isPublic && (
-        <div className='px-6 pb-3 pt-24 -mt-20 bg-black w-full '>
-          <div className='flex justify-between items-center'>
-            <button className='hover:no-underline outline-none' onClick={() => history.push(signout?.link || '/')}>
-              <img
-                className='w-20'
-                src='/DD-Logo-1024x487.png'
-                alt='broken logo'
-              />
-            </button>
-            {signout && (
+    <div className='mb-12'>
+      <div className='h-38 w-full bg-stone z-10'>
+        {!isPublic && (
+          <div
+            className={classnames({
+              'px-6 pt-24 -mt-20 bg-black w-full': true,
+              'pb-12': location.pathname == '/create/sheet',
+              'pb-3': location.pathname !== '/create/sheet',
+            })}
+          >
+            <div className='flex justify-between items-center'>
               <button
-                className='text-off-white font-roboto uppercase hover:text-hover-white hover:no-underline cursor-pointer outline-none'
-                onClick={() => history.push(signout.link)}
+                className='hover:no-underline outline-none'
+                onClick={() => history.push('/')}
               >
-                {signout.text}
+                <img
+                  className='w-20'
+                  src='/DD-Logo-1024x487.png'
+                  alt='broken logo'
+                />
               </button>
+              {signout && (
+                <button
+                  className='text-off-white font-roboto uppercase hover:text-hover-white hover:no-underline cursor-pointer outline-none'
+                  onClick={() => history.push(signout.link)}
+                >
+                  {signout.text}
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+        {subpath && location.pathname !== '/create/sheet' && (
+          <div className='relative px-5 py-6'>
+            <div className='flex w-full justify-between items-center text-sm'>
+              {links
+                .filter((x) => x.link.indexOf(subpath) > -1)
+                .map((x) => (
+                  <div key={x.text}>
+                    <button
+                      className='text-off-white font-roboto uppercase hover:text-hover-white hover:no-underline cursor-pointer outline-none'
+                      onClick={() => history.push(x.link)}
+                    >
+                      {x.text}
+                    </button>
+                  </div>
+                ))}
+            </div>
+            {isPublic && (
+              <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                <img
+                  className='w-24'
+                  src='DD-Logo-1024x487.png'
+                  alt='broken logo'
+                />
+              </div>
             )}
           </div>
-        </div>
-      )}
-      {subpath && location.pathname !== '/create/sheet' && (
-        <div className='relative px-5 py-6'>
-          <div className='flex w-full justify-between items-center text-sm'>
-            {links
-              .filter((x) => x.link.indexOf(subpath) > -1)
-              .map((x) => (
-                <div key={x.text}>
-                  <button 
-                    className='text-off-white font-roboto uppercase hover:text-hover-white hover:no-underline cursor-pointer outline-none'
-                    onClick={() => history.push(x.link)}
-                  >
-                    {x.text}
-                  </button>
-                </div>
-              ))}
-          </div>
-          {isPublic && (
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-              <img
-                className='w-24'
-                src='DD-Logo-1024x487.png'
-                alt='broken logo'
-              />
-            </div>
-          )}
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 }
